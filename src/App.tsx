bf756ceda6766bde5f96ea52adcb59a257ada58e
akolle <andykoller55@yahoo.com>
2024-10-22 19:49:09 -0400

    working with props part 2

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import Component from './starter/02-props/index'
 function App() {
   return (
     <main>
-      <Component name="andy" />
+      <Component name="andy">
+        <h2>hello world</h2>
+      </Component>
     </main>
   )
 }
--- src/starter/02-props/index.tsx
+++ src/starter/02-props/index.tsx
@@ -1,9 +1,13 @@
-function Component({ name }: { name: string }) {
+type ComponentProps = {
+  name: string
+  children: React.ReactNode
+}
+
+function Component({ name, children }: ComponentProps) {
   return (
     <div>
-      <h2>React & Typescript</h2>
-      <h2>Props</h2>
       <h4>this is my name: {name}</h4>
+      {children}
     </div>
   )
 }
