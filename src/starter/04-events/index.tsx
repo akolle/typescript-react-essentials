854d90802747e94778a8ec68e211ba07197f4dd0
akolle <andykoller55@yahoo.com>
2024-10-29 22:30:23 -0400

    handle submit

--- src/starter/04-events/index.tsx
+++ src/starter/04-events/index.tsx
@@ -3,25 +3,37 @@ import { useState } from 'react'
 function Component() {
   const [text, setText] = useState('')
   const [email, setEmail] = useState('')
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     console.log(e.target.value)
     setEmail(e.target.value)
   }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+    // const formData = new FormData(e.target as HTMLFormElement)
+    const data = Object.fromEntries(formData)
+    console.log(data)
+  }
+
   return (
     <section>
       <h2>events example</h2>
-      <form className="form">
+      <form onSubmit={handleSubmit} className="form">
         <input
           type="text"
           className="form-input mb-1"
           value={text}
           onChange={(e) => setText(e.target.value)}
+          name="text"
         ></input>
         <input
           type="email"
           className="form-input mb-1"
           value={email}
           onChange={handleChange}
+          name="email"
         ></input>
         <button type="submit" className="btn btn-block">
           submit
