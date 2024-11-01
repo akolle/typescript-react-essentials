719fd38507cc463e6a36ff138b55d6bb13e837fd
akolle <andykoller55@yahoo.com>
2024-11-01 17:49:30 -0400

    basic context api setup

--- src/starter/06-context/index.tsx
+++ src/starter/06-context/index.tsx
@@ -1,9 +1,21 @@
+import { useTheme, ThemeProvider } from './context'
+
+function ParentComponent() {
+  return (
+    <ThemeProvider>
+      <Component />
+    </ThemeProvider>
+  )
+}
+
 function Component() {
+  const context = useTheme()
+  console.log(context)
   return (
     <div>
       <h2>React & Typescript</h2>
       <h2>Context API</h2>
     </div>
-  );
+  )
 }
-export default Component;
+export default ParentComponent
