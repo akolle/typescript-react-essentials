258bc14e4d68a73418c026ca867129d89fe7d3ad
akolle <andykoller55@yahoo.com>
2024-11-11 11:17:10 -0500

    add local storage and complete project

--- src/starter/10-tasks/index.tsx
+++ src/starter/10-tasks/index.tsx
@@ -3,8 +3,17 @@ import Form from './Form'
 import List from './List'
 import { type Task } from './types'
 
+function loadTasks(): Task[] {
+  const storedTasks = localStorage.getItem('tasks')
+  return storedTasks ? JSON.parse(storedTasks) : []
+}
+
+function updateStorage(tasks: Task[]): void {
+  localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
 function Component() {
-  const [tasks, setTasks] = useState<Task[]>([])
+  const [tasks, setTasks] = useState<Task[]>(() => loadTasks())
 
   const addTask = (task: Task): void => {
     setTasks([...tasks, task])
@@ -20,6 +29,10 @@ function Component() {
       })
     )
   }
+
+  useEffect(() => {
+    updateStorage(tasks)
+  }, [tasks])
 
   return (
     <section>
