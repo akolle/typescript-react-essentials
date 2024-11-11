4a5dc1c195f809934b0163778aec1c7190c924a9
akolle <andykoller55@yahoo.com>
2024-11-11 10:53:16 -0500

    add form functionality

--- src/starter/10-tasks/Form.tsx
+++ src/starter/10-tasks/Form.tsx
@@ -1,4 +1,32 @@
+import { useState } from 'react'
+import { type Task } from './types'
+
 function Form() {
-  return <div>Form</div>
+  const [text, setText] = useState('')
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (!text) {
+      alert('please enter a task')
+      return
+    }
+    // add task
+    console.log(text)
+    setText('')
+  }
+
+  return (
+    <form className="form task-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="form-input"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button type="submit" className="btn">
+        add task
+      </button>
+    </form>
+  )
 }
 export default Form
